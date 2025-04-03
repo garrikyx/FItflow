2bfc03433dbf45eb6c9b2f63f0e83fa999e94bdf
Zephan Chin <zephan.chin.2023@scis.smu.edu.sg>
2025-04-03 14:38:15 +0800

    working twilio

--- backend/composite/HealthDataAggregator/main.py
+++ backend/composite/HealthDataAggregator/main.py
@@ -1,17 +1,20 @@
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv()
 
 message = Mail(from_email = 'ecosmart.diet@gmail.com',
-               to_email = 'zephanchin123@gmail.com',
+               to_emails = 'zephanchin123@gmail.com',
                subject = 'Monthly Report Stats',
                plain_text_content = 'Ran for 30 mins and burnt 120 calories',
-               htmnl_content = '<strong>Ran for 30 mins and burnt 120 calories</strong>')
-try:
-    sg = SendGridAPIClient(os.environ['SENDGRID_API_KEY'])
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
+               html_content = '<strong>Ran for 30 mins and burnt 120 calories</strong>')
+# try:
+sg = SendGridAPIClient(os.environ['SENDGRID_API_KEY'])
+response = sg.send(message)
+print(response.status_code)
+# print(response.body)
+# print(response.headers)
+# except Exception as e:
+#     print(e.message)
