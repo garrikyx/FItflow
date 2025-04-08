b67d1459a8f49db86bfd8fca41826e5b92b8ee3c
garrikyx <garrisonkoh12315@gmail.com>
2025-04-08 16:08:16 +0800

    chore: cleanup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ESD
+# FitFlow
  
--- backend/atomic/User/users.sql
+++ backend/atomic/User/users.sql
@@ -45,7 +45,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`user_id`, `email`, `name`, `password`, `weight`, `goal`) VALUES
 ('4', 'lynn.ong.2023@smu.edu.sg', 'Lynn', '1234', 100, 'lose'),
-('5', 'garrisonkoh.2023@scis.smu.edu.sg','Garrison','1234',70.5, 'gain');
+('5', 'garrisonkoh.2023@scis.smu.edu.sg','Garrison','1234',70.5, 'gain'),
+('6', 'zephanchin@gmail.com','Zephan','123123',80, 'gain'),
+('10', '"jn.tjong.2023@scis.smu.edu.sg"','Tjong Jia Ning','1234',65, 'lose');
 
 
 --
